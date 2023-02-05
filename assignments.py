1ea393a40b0f2d71a84bafe3f3f4407a6f798eba
Jatinkatoch <122670105+Jatinkatoch@users.noreply.github.com>
2023-02-05 22:20:26 +0530

    Add files via upload

---
+++ assignments.py
@@ -0,0 +1,55 @@
+# Q1--> Who developed Python Programming language?
+# Ans--> Guido van Rossum
+
+# Q2--> Which type of programming does python support?
+# Ans--> Python supports Object-oriented programming language and structed programming
+
+# Q3--> Is python case sensitive when dealing with identifiers?
+# And--> Yes
+
+# Q4--> What is the correct extension of the Python file?
+# Ans--> .py is used as extension in Python
+
+#Q5--> Is python code complied or interepreted?
+# Ans--> Python is an interpreted language
+
+# Q6--> Name a few blocks of code used to define in Python language?
+# Ans--> Indentation and Braces{} are used to define a block of code
+
+#Q7--> State a chracter used to give single-line comments in Python?
+# Ans--> #
+
+#Q8-->Mentions functions which can help us to find the version of python that we are currently working on?
+# Ans--> sys.version
+
+#Q9--> Pyton supports the creation of anonymous functions at runtime,using a construct called?
+# Ans--> lambda
+
+#Q10-->What does pip stand for pyton?
+# Ans--> pip is used to install a module in python.
+
+#Q11--> Mention a few built-in functions in python?
+# Ans--> len,sort,add,count,index
+
+#Q12--> What is the maximum possible length of an indentifiers in Python?
+# Ans-->  An identifier can have a maximum length of 79 characters in Python.
+
+# Q13--> What are the benefits of using python?
+# Ans--> 1)Data sciece 
+# 2)Web Development
+# 3)Computer graphic 
+# 4)Basic game development
+# 5)Security and penetration testing
+
+#Q14--> How is memory managed in Python?
+# Ans--> Python involves a private heap containg all Python objects.
+
+#Q15--> How to install Python on Windows and set path variable?
+# Ans-->Right-clicking This PC and going to Properties.
+# Clicking on the Advanced system settings in the menu on the left.
+# Clicking on the Environment Variables button o​n the bottom right.
+# In the System variables section, selecting the Path variable and clicking on Edit.
+
+#Q16--> Is Indentation required in Python?
+# Ans--> Yes, Indentation is required in Python.
+
