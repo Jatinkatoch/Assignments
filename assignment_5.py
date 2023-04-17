acb4471d02a1ced6d4d945ec58e98d00d50f280c
Jatinkatoch <122670105+Jatinkatoch@users.noreply.github.com>
2023-04-17 19:44:25 +0530

    Add files via upload

---
+++ assignment_5.py
@@ -0,0 +1,41 @@
+# Q1 Explain Class and Object-Oriented Programming. Give a suitable example.
+
+# Sol.A class defines object properties including a valid range of values, and a default value. A class also describes object behavior. An object is a member or an "instance" of a class
+
+# create class
+# class Bike:
+#     name = ""
+#     gear = 0
+
+# # create objects of class
+# bike1 = Bike()
+
+# Q2--> Name the four pillars of OOPs
+# The Four Pillars of Object Oriented Programming
+# Abstraction.
+# Encapsulation.
+# Inheritance.
+# Polymorphism.
+
+# Q3. Explain why the __init__() function is used. Give a suitable example.
+
+# Sol.__init__ is a special python method that runs whenever a new object is created.
+# These types of functions are used to initialize the attributes of that class, E.g.,
+#  if the class is Person, then the attributes will be name, age, etc
+
+# Why self is used in OOPs?
+# Sol.self variable is used to represent the instance of the class which is often used in object-oriented programming. 
+
+
+
+
+# Q5. What is inheritance? Give an example for each type of inheritance.
+# Inheritance is a mechanism in which one class acquires the property of another class
+
+# Types of inheritance:-
+# Single Inheritance.
+# Multiple Inheritance.
+# Multilevel Inheritance.
+# Hierarchical Inheritance.
+# Hybrid Inheritance
+
